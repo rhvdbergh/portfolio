f90fa9f4b0f3c46301c444b824b4b9c50ebed7f9
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-12-18 23:31:45 -0800

    react-portfolio build

---
+++ precache-manifest.c7c53e0e2019311899dbcbba4b73c69c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a683071b5e7498ed92174b6b5f225f",
+    "url": "/static/media/lightpaperfibers.90a68307.png"
+  },
+  {
+    "revision": "cd1579a5caea03c03806489d76005217",
+    "url": "/static/media/dry-brush-stroke.cd1579a5.png"
+  },
+  {
+    "revision": "bf895b3712f2aafeb4bbb2b14bf2caec",
+    "url": "/static/media/dry-brush-stroke-38.bf895b37.png"
+  },
+  {
+    "revision": "36930601de29002252a24cab0aa7cf26",
+    "url": "/static/media/dry-brush-stroke-2.36930601.png"
+  },
+  {
+    "revision": "76eeb4148435cd6e08659de596eb5972",
+    "url": "/static/media/dry-brush-stroke-17.76eeb414.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a198438af9ea707d3439",
+    "url": "/static/js/main.a198438a.chunk.js"
+  },
+  {
+    "revision": "fa27ae6c3ae4e66c097e",
+    "url": "/static/js/1.fa27ae6c.chunk.js"
+  },
+  {
+    "revision": "a198438af9ea707d3439",
+    "url": "/static/css/main.037130c3.chunk.css"
+  },
+  {
+    "revision": "41cf2209f1bbc11282e667168749dd06",
+    "url": "/index.html"
+  }
+];
